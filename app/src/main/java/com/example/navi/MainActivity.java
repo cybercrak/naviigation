5540080642daa2e40980fea510af0ff848ff8d7a
cybercrak <nsarran37@gmail.com>
2020-09-24 18:22:06 +0530

    for debugging to mad

--- app/src/main/java/com/example/navi/MainActivity.java
+++ app/src/main/java/com/example/navi/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.navi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    static DrawerLayout drawerlayout;
+    AppBarConfiguration mAppBarConfiguration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       drawerlayout = findViewById(R.id.drawer);
+       findViewById(R.id.burger).setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+              drawerlayout.openDrawer(GravityCompat.START);
+           }
+       });
+
+        NavigationView navigationView=findViewById(R.id.navigation1);
+        NavController navController= Navigation.findNavController(this,R.id.frag);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id., R.id.,R.id.).build();
+       NavigationUI.setupWithNavController(navigationView,navController);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+
+       NavController navController = Navigation.findNavController(this, R.id.frag);
+        return NavigationUI.navigateUp(navController,mAppBarConfiguration)||super.onSupportNavigateUp();
     }
 }
